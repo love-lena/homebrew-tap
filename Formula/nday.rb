b2cd823ca164dd17c5a49b9d626cb260dc912fb9
Lena Hickson Long <79774482+love-lena@users.noreply.github.com>
2022-06-07 23:18:34 -0700

    Update nday.rb

--- Formula/nday.rb
+++ Formula/nday.rb
@@ -1,7 +1,7 @@
 class Nday < Formula
   desc "CLI tool for simple daily notes"
   homepage "https://github.com/love-lena/nday"
-  url "https://github.com/love-lena/nday/releases/download/v0.2.0/nday-0.2.1-x86_64-apple-darwin.tar.gz"
+  url "https://github.com/love-lena/nday/releases/download/v0.2.1/nday-0.2.1-x86_64-apple-darwin.tar.gz"
   sha256 "778ce16be23508d929b7696df46ec6387a05437238690b3087d1bd5fecc5589b"
   version "0.2.1"
 
